77b19d145668246ea1058ef70d694a43a6d2d992
a6h6405wjh <net.junk@hotmail.com>
2019-02-16 20:00:44 +1300

    Release Candidate

--- web/default.js
+++ web/default.js
@@ -6,13 +6,18 @@ var ntp = ntp ||
         ignore: false,
         parentId: "1",
         id: "1",
-        pinned: 0
+        pinned: 0,
+        settingsLoaded: false,
+        folder: '#folderA',
+        hidePinned: true,
+        positionRecall: 0
     };
+
+
 
 ntp.nodeToHtml = function (node) {
     var a = document.createElement("a");
     var label = document.createElement("span");
-    var img = document.createElement("img");
     var title;
 
     a.classList += "book";
@@ -22,8 +27,16 @@ ntp.nodeToHtml = function (node) {
 
     // Process folder.
     if (node.hasOwnProperty("children")) {
-        img.src = "img/folder.svg";
+
+        var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        var use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+
+        use.setAttribute("href", ntp.folder);
+        a.classList.add('folder');
+        
         a.addEventListener("click", ntp.parseNodes);
+        svg.appendChild(use);
+        a.appendChild(svg);
 
         a.oncontextmenu = function (ev) {
             var cmenu = document.getElementById("cmenu");
@@ -47,12 +60,14 @@ ntp.nodeToHtml = function (node) {
         };
     }
     else {
+        var img = document.createElement("img");
         img.src = "chrome://favicon/size/48/" + node.url;
         a.href = node.url;
+        a.appendChild(img);
     }
 
     label.appendChild(document.createTextNode(title));
-    a.appendChild(img);
+    
     a.appendChild(label);
     a.setAttribute("draggable", "true");
     a.addEventListener("dragstart", ntp.dragStart);
@@ -62,8 +77,13 @@ ntp.nodeToHtml = function (node) {
     return a;
 };
 
+
+
 ntp.parseNodes = function (e, pinned, id, up) {
     id = e ? e.currentTarget.id : id ? id : "1";
+
+    if (ntp.positionRecall === 2) chrome.storage.sync.set({ folderId: id }, null);
+    if (ntp.positionRecall === 1) sessionStorage.setItem("folderId", id);
 
     var node = chrome.bookmarks.getSubTree(id,
         function (node) {
@@ -92,16 +112,29 @@ ntp.parseNodes = function (e, pinned, id, up) {
                 nav.appendChild(nodeset);
 
                 nodeset.classList += up ? "up" : "down  ";
+
+                //Insert empty folder notification.
+                if (nodes.length === 0) {
+
+                    var fake = ntp.nodeToHtml({ id: node[0].id, title: 'Empty', children: [null] });
+
+                    fake.classList.add("emptyFolder");
+
+                    // Create a fake node.
+                    nodeset.appendChild(fake);
+                }
             }
 
             for (var i = 0; i < nodes.length; i++) {
-                if (nodes[i].id !== ntp.pinned) {
+                if (nodes[i].id !== ntp.pinned | !ntp.hidePinned) {
                     nodeset.appendChild(ntp.nodeToHtml(nodes[i]));
                 }
             }
         }
     );
 };
+
+
 
 ntp.upHierarchy = function (e) {
     setTimeout(function () { ntp.ignore = false; }, 500);
@@ -112,13 +145,19 @@ ntp.upHierarchy = function (e) {
     }
 };
 
+
+
 ntp.dragStart = function () {
     //console.log('dragstart');
 };
 
+
+
 ntp.dragStop = function () {
     //console.log('dragstop');
 };
+
+
 
 ntp.initialize = function () {
     var link = document.getElementById('mid');
@@ -129,6 +168,33 @@ ntp.initialize = function () {
     link.querySelector('#incognito-link').addEventListener('click', () => {
         chrome.windows.create({ incognito: true, state: "maximized" });
     });
+
+    // Load settings popout.
+    link.querySelector('#settings-link').addEventListener('click', () => {
+
+        if (ntp.settingsLoaded === false) {
+            ntp.openSubPage('settings.html', 'popout', () => {
+
+                ntp.loadSettings();
+                document.getElementById("popout").classList.toggle("out");
+            }
+            );
+        }
+        else {
+            document.getElementById("popout").classList.toggle("out");
+        }
+    });
+
+    ntp.applySettings(true);
+
+    document.getElementById("navigator").addEventListener("wheel", ntp.upHierarchy, false);
+
+    ntp.openSubPage("img/icons.svg", "icons");
+};
+
+
+// Apply settings - parse - boolean, optionally parse nodes.
+ntp.applySettings = function (parse) {
 
     //Get stored settings.
     chrome.storage.sync.get(null, (items) => {
@@ -141,19 +207,162 @@ ntp.initialize = function () {
             document.getElementById("initialTip").style.display = "block";
         }
         else {
-            ntp.parseNodes(undefined, true, ntp.pinned);
+            if (parse) ntp.parseNodes(undefined, true, ntp.pinned);
         }
+
 
         var root = document.documentElement;
 
-        root.style.setProperty("--background", items.background);
-        root.style.setProperty("--foreground", items.foreground);
-        root.style.setProperty("--foregroundAlpha", items.foregroundAlpha / 100);
+        try {
+
+            // Set whatever is required from the settings recieved.
+            root.style.setProperty("--background", items.background);
+
+            root.style.setProperty("--hover", ntp.hexToRGBA(items.hover, items.hoverAlpha));
+
+            root.style.setProperty("--control", ntp.hexToRGBA(items.control, items.controlAlpha));
+
+            root.style.setProperty("--foreground", ntp.hexToRGBA(items.foreground, items.foregroundAlpha));
+
+            root.style.setProperty("--folder", ntp.hexToRGBA(items.folder, items.folderAlpha));
+
+            root.style.setProperty("--iconSize", items.iconSize + 'px');
+
+            root.style.setProperty("--viewWidth", items.viewWidth + 'vw');
+
+            ntp.hidePinned = items.hidePinned;
+
+            ntp.positionRecall = items.positionRecall;
+        }
+        catch (e) { console.log(e); }
+
+        if (parse) {
+
+            var recallId;
+
+            // Parse the initial book set, depending on positionRecall.
+            switch (ntp.positionRecall) {
+                case 0:
+                    {
+                        ntp.parseNodes();
+                        break;
+                    }
+                case 1:
+                    {
+                        recallId = sessionStorage.getItem("folderId");
+                        ntp.parseNodes(undefined, false, recallId);
+                        break;
+                    }
+                case 2:
+                    {
+                        recallId = items.folderId;
+                        ntp.parseNodes(undefined, false, recallId);
+                        //ntp.parseNodes();
+                        break;
+                    }
+            }
+        }
     });
-
-
-    ntp.parseNodes();
-    document.getElementById("navigator").addEventListener("wheel", ntp.upHierarchy, false);
 };
+
+
+//Convert Hex to RGBA
+ntp.hexToRGBA = function (hex, a) {
+
+    var rgba =
+    {
+        r: parseInt(hex.substring(1, 3), 16),
+        g: parseInt(hex.substring(3, 5), 16),
+        b: parseInt(hex.substring(5, 7), 16),
+        a: a / 100
+    };
+
+    return 'rgba(' + rgba.r + ',' + rgba.g + ',' + rgba.b + ',' + rgba.a + ')';
+};
+
+
+
+// Load page within page.
+ntp.openSubPage = function (page, targetId, callback) {
+
+    var xmlReq = new XMLHttpRequest();
+
+    xmlReq.onreadystatechange = function () {
+
+        if (this.readyState === 4 && this.status === 200) {
+
+            var xml = xmlReq.responseText;
+
+            var target = document.getElementById(targetId);
+
+            target.innerHTML = xml;
+
+            xmlReq = undefined;
+
+            if (callback) callback();
+        }
+    };
+
+    xmlReq.open("GET", page, true);
+    xmlReq.send();
+};
+
+
+ntp.loadSettings = function () {
+
+    //Get stored settings.
+    chrome.storage.sync.get(null, (items) => {
+        var root = document.documentElement;
+
+        var inputs = document.querySelectorAll("#settings input, #settings select");
+
+        function initInput(key) {
+
+            var inp;
+
+            //Match key in nodelist.
+            for (let x = 0; x < inputs.length; x++) {
+                if (inputs[x].id === key) {
+                    inp = inputs[x];
+                    break;
+                }
+            }
+
+
+            try {
+                if (items[key]) inp.value = items[key];
+
+                // Special case for checked.
+                if (inp.type === "checkbox") inp.checked = items[key];
+            }
+            catch (e) { console.log(e); }
+
+
+            // Store the control value.
+            if (inp) inp.addEventListener("input", () => {
+                var val = inp.type === 'checkbox' ? inp.checked : inp.value;
+                chrome.storage.sync.set({ [key]: val}, null);
+                ntp.applySettings();
+            });
+        }
+
+        initInput("background");
+        initInput("hover");
+        initInput("hoverAlpha");
+        initInput("control");
+        initInput("control1Alpha");
+        initInput("foreground");
+        initInput("foregroundAlpha");
+        initInput("folder");
+        initInput("folderAlpha");
+        initInput("iconSize");
+        initInput("viewWidth");
+        initInput("hidePinned");
+        initInput("positionRecall");
+
+        document.getElementById("closeSettings").addEventListener('click', () => { document.getElementById('popout').classList.remove('out'); });
+    });
+};
+
 
 document.addEventListener('DOMContentLoaded', ntp.initialize);
--- web/settings.js
+++ web/settings.js
@@ -8,7 +8,7 @@ var ntp = ntp ||
 
 
 
-ntp.initSettings = function () {
+ntp.loadSettings = function () {
 
     //Get stored settings.
     chrome.storage.sync.get(null, (items) =>
@@ -21,17 +21,44 @@ ntp.initSettings = function () {
         {
             var inp = inputs.namedItem(key);
 
-            if (items[key]) inp.value = items[key];
+            try {
+                if (items[key]) inp.value = items[key];
+            }
+            catch (e) { console.log(e); }
+           
 
             inp.addEventListener("change", () => {
                 chrome.storage.sync.set({ [key]: inp.value }, null);
+                ntp.applySettings();
             });
         }
 
         initInput("background");
         initInput("foreground");
         initInput("foregroundAlpha");
+        initInput("backgroundImage");
+
+        document.getElementById("bgImageUrl").innerText = items.backgroundImage.replace("C:\\fakepath\\", "");
+
+        document.getElementById("backgroundImage").addEventListener("change", previewFile);
+        previewFile(items.backgroundImage);
     });
 };
 
-document.addEventListener('DOMContentLoaded', ntp.initSettings);
+
+function previewFile(img)
+{
+    var preview = document.querySelector('#pvImg');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        preview.src = reader.result;
+    }, false);
+
+    if (img) {
+        reader.readAsDataURL(img);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', ntp.loadSettings);
